613d29630ddc9e96d1f44b9dfbbd9d4c1eb7bc6d
rohitmaxup <zunzunwalarohit@gmail.com>
2026-05-31 19:43:46 +0530

    Add Collatz Conjecture [A] from Codeforces

---
+++ 2137/A - Collatz Conjecture/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+ 
+    while (t--)
+    {
+        int k, x;
+        cin >> k >> x;
+ 
+        while (k--)
+        {
+            if ((x - 1) % 3 == 0 && ((x - 1) / 3) % 2 == 1)
+            {
+                x = (x - 1) / 3;
+            }
+            else
+            {
+                x = x * 2;
+            }
+        }
+ 
+        cout << x << endl;
+    }
+}
