aa280b1292f8fd4e7767e6bef6338d930f696634
rohitmaxup <zunzunwalarohit@gmail.com>
2026-05-31 19:40:13 +0530

    Add Shizuku Hoshikawa and Farm Legs [A] from Codeforces

---
+++ 2171/A - Shizuku Hoshikawa and Farm Legs/solution.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        if (n % 2 == 1)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            cout << (floor(n / 4) + 1) << endl;
+        }
+    }
+}
