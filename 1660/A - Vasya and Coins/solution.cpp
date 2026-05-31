c89d165607b16b7acbeddc80c9ca9d31d7fb02ff
rohitmaxup <zunzunwalarohit@gmail.com>
2026-05-31 19:42:55 +0530

    Add Vasya and Coins [A] from Codeforces

---
+++ 1660/A - Vasya and Coins/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long a ,b ;
+        cin>>a>>b;
+        
+        if(a==0)
+        {
+            cout<<1<<endl;
+        }
+        else
+        {
+            cout<<(long long)(a+2*b+1)<<endl;
+        }
+    }
+}
