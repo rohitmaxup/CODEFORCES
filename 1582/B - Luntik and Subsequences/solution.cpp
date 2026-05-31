55a400bd19ee8b27bb1b134819d9732092b202e3
rohitmaxup <zunzunwalarohit@gmail.com>
2026-05-31 19:40:02 +0530

    Add Luntik and Subsequences [B] from Codeforces

---
+++ 1582/B - Luntik and Subsequences/solution.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+ 
+        vector<int> v(n);
+ 
+        for(int i = 0 ; i < n ; i++)
+        {
+            cin>>v[i];
+        }
+        int r = count(v.begin(),v.end(),0);
+        int k = count(v.begin(),v.end(),1);
+ 
+        cout<<k*(1ll<<r)<<endl;
+ 
+ 
+    }
+}
