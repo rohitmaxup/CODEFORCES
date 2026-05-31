f24fadcfeedaef555fff05cc256f2f2ac902092d
rohitmaxup <zunzunwalarohit@gmail.com>
2026-05-31 19:33:21 +0530

    Add Goals of Victory [A] from Codeforces

---
+++ 1877/A - Goals of Victory/solution.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+ 
+        int sum = 0;
+        int arr[n-1];
+        for(int i = 0; i <n-1 ; i++ )
+        {
+            cin>>arr[i];
+ 
+            sum = sum + arr[i];
+        }
+ 
+        cout<<(-1)*sum<<endl;
+    }
+}
