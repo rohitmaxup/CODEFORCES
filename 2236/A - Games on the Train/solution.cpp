77de1991cbaabe85822b8c8ad865708d110b4d18
Rohit Zunzunwala <zunzunwalarohit@gmail.com>
2026-06-12 20:30:50 +0530

    Add Games on the Train [A] from Codeforces

---
+++ 2236/A - Games on the Train/solution.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        int mn = 1e9, mx = 0;
+ 
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            mn = min(mn, x);
+            mx = max(mx, x);
+        }
+ 
+        cout << mx - mn + 1 << '
+';
+    }
+ 
+    return 0;
+}
